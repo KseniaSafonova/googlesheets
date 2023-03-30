28f65ec9c785575a33ed28f0a90f531bbc003a97
ksenias <kseniatsn@gmail.com>
2023-03-30 18:45:27 +0300

    function

--- react-googlesheets/src/AddForm.js
+++ react-googlesheets/src/AddForm.js
@@ -8,70 +8,48 @@ import axios from 'axios';
 
 const titles = ['Машина', 'Номер', 'Имя', 'Телефон', 'Квартира', 'Сатус оплаты']
 
-// function AddForm() {
-//     const [carValue, setCarValue] = useState('');
-//     const [numberValue, setNumberValue] = useState('');
-//     const [nameValue, setNameValue] = useState('');
-//     const [phoneValue, setPhoneValue] = useState('');
-//     const [apartmentValue, setApartmentValue] = useState('');
-//     const [paymentValue, setPaymentValue] = useState('');
+function AddForm() {
+    const [carValue, setCarValue] = useState('');
+    const [numberValue, setNumberValue] = useState('');
+    const [nameValue, setNameValue] = useState('');
+    const [phoneValue, setPhoneValue] = useState('');
+    const [apartmentValue, setApartmentValue] = useState('');
+    const [paymentValue, setPaymentValue] = useState('');
 
-// const HandleChangeCar = (event) => {
-//     setCarValue(event.target.value)
-// }
-
-// const HandleChangeNumber = (event) => {
-//     setNumberValue(event.target.value)
-// }
-
-// const HandleChangeName = (event) => {
-//     setNameValue(event.target.value)
-// }
-
-// const HandleChangePhone = (event) => {
-//     setPhoneValue(event.target.value)
-// }
-
-// const HandleChangeApartment = (event) => {
-//     setApartmentValue(event.target.value)
-// }
-
-
-
-export default class AddForm extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            carValue: '',
-            numberValue: '',
-            nameValue: '',
-            phoneValue: '',
-            apartmentValue: ''
-        }
+    const HandleChangeCar = (event) => {
+        setCarValue(event.target.value)
     }
 
-    HandleChange = (event) => {
-        this.setState({ [event.target.name]: event.target.value })
+    const HandleChangeNumber = (event) => {
+        setNumberValue(event.target.value)
     }
 
-    HandleSubmit = (event) => {
-        event.preventDefault();
-        console.log(this.state);
-
-        axios.post('https://sheet.best/api/sheets/0b377265-6ca4-4cec-ad98-c4bbcacc31ff', this.state)
-            .then(response => {
-                console.log(response);
-            })
+    const HandleChangeName = (event) => {
+        setNameValue(event.target.value)
     }
 
+    const HandleChangePhone = (event) => {
+        setPhoneValue(event.target.value)
+    }
 
-    // return (
-    render() {
-        const { carValue, numberValue, nameValue, phoneValue, apartmentValue } = this.state;
-        return (
-            <>
-                {/* <h2>Добавить позицию</h2>
+    const HandleChangeApartment = (event) => {
+        setApartmentValue(event.target.value)
+    }
+
+    // HandleSubmit = (event) => {
+    //     event.preventDefault();
+    //     console.log(this.state);
+
+    //     axios.post('https://sheet.best/api/sheets/0b377265-6ca4-4cec-ad98-c4bbcacc31ff', this.state)
+    //         .then(response => {
+    //             console.log(response);
+    //         })
+    // }
+
+
+    return (
+        <>
+            {/* <h2>Добавить позицию</h2>
             {
                 titles.map((title) =>
                     <div>
@@ -81,35 +59,36 @@ export default class AddForm extends Component {
                 )
             } */}
 
-                <Form onSubmit={this.HandleSubmit}>
-                    <Form.Field>
-                        <label>Машина</label>
-                        <input onChange={this.HandleChange} value={carValue} name='carValue' />
-                    </Form.Field>
-                    <Form.Field>
-                        <label>Номер</label>
-                        <input onChange={this.HandleChange} value={numberValue} name='numberValue' />
-                    </Form.Field>
-                    <Form.Field>
-                        <label>Имя</label>
-                        <input onChange={this.HandleChange} value={nameValue} name='nameValue' />
-                    </Form.Field>
-                    <Form.Field>
-                        <label>Телефон</label>
-                        <input onChange={this.HandleChange} value={phoneValue} name='phoneValue' />
-                    </Form.Field>
-                    <Form.Field>
-                        <label>Квартира</label>
-                        <input onChange={this.HandleChange} value={apartmentValue} name='apartmentValue' />
-                    </Form.Field>
-                    {/* <Form.Field>
+            <Form onSubmit={HandleSubmit}>
+                <Form.Field>
+                    <label>Машина</label>
+                    <input onChange={HandleChangeCar} value={carValue} name='carValue' />
+                </Form.Field>
+                <Form.Field>
+                    <label>Номер</label>
+                    <input onChange={HandleChangeNumber} value={numberValue} name='numberValue' />
+                </Form.Field>
+                <Form.Field>
+                    <label>Имя</label>
+                    <input onChange={HandleChangeName} value={nameValue} name='nameValue' />
+                </Form.Field>
+                <Form.Field>
+                    <label>Телефон</label>
+                    <input onChange={HandleChangePhone} value={phoneValue} name='phoneValue' />
+                </Form.Field>
+                <Form.Field>
+                    <label>Квартира</label>
+                    <input onChange={HandleChangeApartment} value={apartmentValue} name='apartmentValue' />
+                </Form.Field>
+                {/* <Form.Field>
                     <label>Статус оплаты</label>
                     <input onChange={HandleChange} value={paymentValue}/>
                 </Form.Field> */}
-                    <Button basic>Отменить</Button>
-                    <Button type='submit' basic color='violet'>Отправить</Button>
-                </Form>
-            </>
-        )
-    }
+                <Button basic>Отменить</Button>
+                <Button type='submit' basic color='violet'>Отправить</Button>
+            </Form>
+        </>
+    )
 }
+
+export default AddForm;
